0305909160fcaca770b85622be9bb0bda4bea332
Tentacle-Monster <eigonofcarim@gmail.com>
2019-04-15 23:01:04 +0300

    writing to file added

--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 const xlsx = require("node-xlsx");
 const fs = require("fs");
+const docx = require("docx");
 
 const input = document.querySelector("#fileInput");
 
@@ -8,18 +9,20 @@ input.addEventListener("change", () => {
   const { path, name } = file;
   setChosenFiles(name)
   const spreadsheetData = getSpreadsheetData({ spreadsheetPath: path });
+  var resultString='';
 
-  // dropArea.style.border = '2px dashed #00FF93'
-  // dropArea.style.background = 'rgba(0, 255, 147, 0.44)'
-  //
-  // label.style.background = 'rgba(0, 255, 147, 0.44)'
-
+ 
   traverseArrayAndFindRows(spreadsheetData);
   const departmentName = spreadsheetData[1][1];
+ 
+  const coreName = spreadsheetData[6][1];
   const assets = getAssets({ spreadsheetData });
   const assetsCount = getAssetsNum({ spreadsheetData });
   const assetsPositionsThatExist = getAssetsPositionThatExist({ assetsCount });
   logAssetsThatExist({ assetsPositionsThatExist, assets });
+
+  resultString += departmentName + ',' + coreName + ',' + arrayAssetsThatExist({ assetsPositionsThatExist, assets }).join(',');
+  createdoc(name, resultString);
 });
 
 /********* HELPERS METHODS **********************/
@@ -59,6 +62,11 @@ function getAssetsPositionThatExist({ assetsCount }) {
 function logAssetsThatExist({ assetsPositionsThatExist, assets }) {
   assetsPositionsThatExist.forEach(position => console.log(assets[position]));
 }
+function arrayAssetsThatExist({ assetsPositionsThatExist, assets }) {
+  var out= new Array();
+  assetsPositionsThatExist.forEach(position => out.push(assets[position]));
+  return out;
+}
 
 function getTotalCountRowNum(rowValue, rowIndex) {
   if (typeof rowValue === "string") {
@@ -85,6 +93,28 @@ function traverseArrayAndFindRows(array) {
     }
   });
 }
+function createdoc(name, text){
+  //style example
+  var doc = new docx.Document();
+  doc.Styles.createParagraphStyle('wellSpaced', 'Well Spaced')
+    .basedOn('Normal')
+    .color('999999')
+    .italics()
+    .spacing({ line: 276, before: 20 * 72 * .1, after: 20 * 72 * .05 });
+  // Add some content in the document
+  var paragraph = new docx.Paragraph("Хуй блять его знает, что сюда писать. Надо взять док").style('wellSpaced');
+  doc.addParagraph(paragraph);
+  if (typeof text === "string") 
+  doc.addParagraph(new docx.Paragraph(text));
+  var packer = new docx.Packer();
+  var newName= 'file';
+  if (typeof name === "string") newName = name.split('.')[0];
+  //console.log(name.split('.'));
+  packer.toBuffer(doc).then((buffer) => {
+    fs.writeFileSync(newName+".docx", buffer);
+  });
+
+}
 
 /******* UI METHODS AND IMPLEMENTATIONS ***********/
 
@@ -99,3 +129,4 @@ var label = document.querySelector("label");
 dropArea.addEventListener('dragenter', () => {
   dropArea.style.background = 'rgba(255, 255, 255, 0.2)'
 })
+
